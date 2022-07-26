f45f7a51d5d2031430830ed8bf97b545162c75e4
Emrah <emrahkbulut@gmail.com>
2022-07-26 23:55:18 +0300

    xcode project created

---
+++ RaccoonGram/RaccoonGram/RaccoonGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccoonGramApp.swift
+//  RaccoonGram
+//
+//  Created by Emrah Karabulut on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RaccoonGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
